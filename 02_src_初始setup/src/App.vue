7b50258607beebba754dc2b1ccba1ed6a89928fc
wulin <linwu0586@outlook.com>
2024-02-16 21:20:38 +0900

    three commit

---
+++ 02_src_初始setup/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名: {{ name }}</h2>
+  <h2>年龄: {{ age }}</h2>
+  <h2>性别: {{ gender }}</h2>
+  <h2>a的值是: {{ a }}</h2>
+  <button @click="sayHello">说话(Vue3所配置的--sayHello)</button>
+  <br>
+  <br>
+  <button @click="sayWelcome">说话(Vue2所配置的--sayWelcome)</button>
+  <br>
+  <br>
+  <button @click="vue2_test">测试一下在Vue2的配置中去读取Vue3的数据、方法</button>
+  <br>
+  <br>
+  <button @click="vue3_test">测试一下在Vue3的setup配置中去读取Vue2的数据、方法</button>
+
+</template>
+
+<script>
+// import {h} from 'vue'
+
+export default {
+  name: 'App',
+  // vue2的写法：直接写到{}里,所以都是key-value键值对的写法。data是因为简写方式,原型是 data: function(){...}
+  data() {
+    return {
+      gender: '男',
+      a: 100
+    }
+  },
+  methods: {
+    sayWelcome(){
+      alert('欢迎来到凤山学前端~')
+    },
+
+    vue2_test(){
+      console.log(this.gender)
+      console.log(this.a)
+      // 下面三个是在vue3的setup中定义的
+      console.log(this.name)
+      console.log(this.age)
+      this.sayHello()
+    }
+  },
+
+
+  // vue3的setup写法：所有的内容都写入到setup(){...} 函数体中
+  setup() {
+    // 数据
+    let name = '张三'
+    let age = 18
+
+    // 方法
+    function sayHello() {
+      alert(`我叫${name}, 今年${age}岁了。正在苦学前端Vue~`)
+    }
+
+    function vue3_test() {
+      // 自己这边的name、age、sayHello就不用this了,因为是在函数体里的~~~
+      console.log(name)
+      console.log(age)
+      sayHello()
+
+      //gender和a是在vue2中定义的
+      console.log(this.gender)
+      console.log(this.a)
+      this.sayWelcome()
+    }
+
+    // 必须将定义的数据返回出去
+    return {
+      name,
+      age,
+      sayHello,
+      vue3_test,
+    }
+
+    // return ()=>{ return h('h1', '凤山大学')}
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 03_src_ref函数/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名: {{ name }}</h2>
+  <h2>年龄: {{ age }}</h2>
+  <h3>工作种类：{{ job.type }}</h3>
+  <h3>工作薪水：{{ job.salary }}</h3>
+  <button @click="changeInfo">修改人的信息</button>
+
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  name: 'App',
+  // vue3的setup写法：所有的内容都写入到setup(){...} 函数体中
+  setup() {
+    // ref定义响应式数据： RefImpl: reference(引用)、implement(实现)
+    let name = ref('张三')
+    let age = ref(18)
+    let job = ref({
+      type:'码农',
+      salary: '30k'
+    })
+
+    // 方法
+    function changeInfo(){
+      name.value = '李四'
+      age.value = 48
+      job.value.type = '系统架构师'
+      job.value.salary = 99
+      console.log(name,age.value)
+    }
+
+    // 必须将定义的数据返回出去
+    return {
+      name,
+      age,
+      changeInfo,
+      job,
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
